dfe060f596d63a74473d32108a51c2340193c24a
Ellen Åkerlind <ellenviola04@outlook.com>
2024-02-26 14:52:24 +0100

    Samlat alla funktioner i en funktion

--- booking_prompts.ts
+++ booking_prompts.ts
@@ -1,33 +1,22 @@
-
-
 import * as promptSync from "prompt-sync";
 import { Person, Reservation, find_unbooked, leave_spot, make_booking, park_at } from "./project_booking";
-import { get_park_from_parkingLots } from "./parking_lots";
+import { get_park_from_parkingLots, set_up } from "./parking_lots";
 
 
 const prompt = promptSync(); 
-//Har id check!
 
-//Fråga när den vill parkera (kolla när ok) ckeck!
-
-//Fråga vart den vill parkera check!
-
-//Plocka fram parkering (tillåt ej "test" eller "empty") check!
-
-//Kolla om den är full 
-
-//Om full gå vidare till att boka 
-
-//Om den är tom gå vidar till att skriva in parkering igen (spara datum)
-
-//Fråga om den vill parkera, om ja parkera 
-
-//Fråga om den vill lämna parkering, om ja lämna 
-
-//Funktioner: ask_park, ask_date, book
+//OBS OBS OM DU TEST-KÖR OCH VILL ANVÄNDA SAMMA INFO, använd set_up
+set_up();
 
 const personID = 20040520 //TEMPORÄRT!!!
 
+/**
+ * Asks the user for a start and an end date, keeps asking untill it gets a
+ * valid date. When the user has given a correct date the function returns 
+ * a reservation (a record with the start date, end date and the user's ID)
+ * @param person The user's id
+ * @returns 
+ */
 function ask_date(person: Person): Reservation{
     function is_invalid_date(dateS: Date, dateE: Date):boolean {
         if(dateS.toString() === "Invalid Date" || 
@@ -53,6 +42,12 @@ function ask_date(person: Person): Reservation{
             dateEnd: dateEnd}
 }
 
+/**
+ * 
+ * @param dateS 
+ * @param dateE 
+ * @param person 
+ */
 function ask_park(dateS, dateE, person): void {
     let spot = NaN; //Temporärt 
     const file = "saved_parking_lots.json";
@@ -96,6 +91,10 @@ function ask_park(dateS, dateE, person): void {
     }
 }
 
+/**
+ * 
+ * @param personID 
+ */
 function parking(personID): void {
     let validAnsw = false;
     while(!validAnsw) {
@@ -111,11 +110,15 @@ function parking(personID): void {
         //Valt en parkering som finns 
         } else {
             const spot = Number(prompt("At what spot? "));
-            validAnsw = park_at(park, spot, personID); //Lite förvirrande kanske, men om den är true så har man lyckats parkera
+            validAnsw = (park_at(park, spot, personID)); //Lite förvirrande kanske, men om den är true så har man lyckats parkera
         }
     }
 }
 
+/**
+ * 
+ * @param personID 
+ */
 function leave(personID): void {
     let validAnsw = false;
     while(!validAnsw) {
@@ -137,17 +140,45 @@ function leave(personID): void {
 }
 
 //OM MAN VÄLJER ATT BOKA: 
-const reservation = ask_date(personID);
+//const reservation = ask_date(personID);
 
-ask_park(reservation.dateStart, reservation.dateEnd, personID);
+//ask_park(reservation.dateStart, reservation.dateEnd, personID);
 
 //OM MAN VÄLJER ATT PARKERA: 
 //Ta fram reservation (OBS! måste veta parkering och parkerings-plats)
 //Lättast för oss om användaren bara skriver in vart den vill parkera, men 
 //det blir inte lättast för användaren eftersom den måste komma ihåg allt 
-parking(personID);
+//parking(personID);
 
 
 //OM MAN VÄLJER ATT LÄMNA PARKERING 
 //samma som när man väljer att parkera, lättare för oss om användaren skriver in själv 
-leave(personID);
+//leave(personID);
+
+function park_options(personID: number): void{
+    let validUserAnswer = false;
+    while(validUserAnswer === false) {
+        console.log("What do you want to do?");
+        console.log("Options:");
+        console.log("book a spot: B");
+        console.log("park at your booked spot: P");
+        console.log("leave your parking spot: L");
+        const answ = prompt("").toLocaleLowerCase(); 
+        if(answ === "b") {
+            const reservation = ask_date(personID);
+            ask_park(reservation.dateStart, reservation.dateEnd, personID);
+            console.log("Your booking is registered");
+            validUserAnswer = true;
+        } else if(answ === "p") {
+            parking(personID);
+            console.log("You have parked at your spot")
+            validUserAnswer = true;
+        } else if(answ === "l") {
+            leave(personID);
+            console.log("You have left your spot")
+            validUserAnswer = true;
+        } else {
+            console.log("Invalid answer, please try again")
+        }
+    }
+}
